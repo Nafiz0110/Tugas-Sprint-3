dce61337f229db551edc561ae2a2f180ccbf0caa
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-08-04 21:09:08 +0700

    Ada yang salah hehe

--- Tugas 2/Nomer 2.js
+++ Tugas 2/Nomer 2.js
@@ -1,7 +1,10 @@
-//NOMER 2
-var kodeUnixRandom = [
-  12, 78, 42, 40, 36, 2, 8, 96, 66, 20, 91, 85, 41, 69, 38, 77, 97, 60, 15, 57,
-];
+let CallbackFilter = () => {
+  kodeUnixRandom()
+}
 
-const NilaiData = kodeUnixRandom.filter((a) => a >= 60);
-console.log(NilaiData);
+let kodeUnixRandom = () => {
+  var kodeUnix = [23, 89,67,29,192,6,2,129,21,872,891,901,70,61,78,62,32,90,90];
+  console.log(kodeUnix.filter((a) => a >= 60));
+}
+
+CallbackFilter(kodeUnixRandom)
