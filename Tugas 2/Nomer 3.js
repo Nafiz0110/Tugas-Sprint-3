c642e8456ff8215340520f9147624c844217476d
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-08-04 21:16:06 +0700

    Ada yang salah hehe

--- Tugas 2/Nomer 3.js
+++ Tugas 2/Nomer 3.js
@@ -1,5 +1,10 @@
-var kodeUnix = [
-  23, 89, 67, 29, 192, 6, 2, 129, 21, 872, 891, 901, 70, 61, 78, 62, 32, 90, 90,
-];
-var hasil = kodeUnix.map((a) => (a * 2) / 5 - kodeUnix.length);
-console.log(hasil);
+let filterCallback = () => {
+  kodeUnixRandom()
+}
+
+let kodeUnixRandom = () => {
+  var kodeUnix = [23, 89, 67, 29, 192, 6, 2, 129, 21, 872, 891, 901, 70, 61, 78, 62, 32, 90, 90,];
+  console.log(kodeUnix.map((a) => (a * 2) / 5 - kodeUnix.length));
+}
+
+filterCallback(kodeUnixRandom)
