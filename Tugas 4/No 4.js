6e15bd53c62657649936bd6530f1ade2643454ac
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-08-06 22:36:32 +0700

    Hemm bisa

--- Tugas 4/No 4.js
+++ Tugas 4/No 4.js
@@ -1,1 +1,20 @@
-z
+var statsEmail = false;
+var statsPassword = false;
+
+function login(email, password) {
+  if (email == "nafiz@gmail.com") {
+    if (password == "nafiz") {
+      document.write("halaman home");
+      statsEmail = true;
+      statsPassword = true;
+    } else {
+      document.write("password yang anda masukkan salah");
+    }
+  } else {
+    document.write("email yang anda masukkan salah");
+  }
+}
+
+do {
+  login(prompt("masukkan email anda"), prompt("msukkan password anda"));
+} while (!statsEmail && !statsPassword);z
